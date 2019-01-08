90adf76096c7d8c94c5706fcc278f7b3c2923b89
barman47 <nomsouzoanya@yahoo.co.uk>
2019-01-08 13:23:17 +0100

    Added SSL Certificate

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const exphbs = require('express-handlebars');
 const favicon = require('express-favicon');
 const path = require('path');
 
+const sgMail = require('@sendgrid/mail');
+
 const PORT  = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, 'public')
 
@@ -56,19 +58,39 @@ app.post('/email', (req, res) => {
     //     }
     // });
 
-    
-    const transporter = nodemailer.createTransport({
-        // service: 'gmail',
-        host: 'smtp.gmail.com',
-        port: 465,
-        auth: {
-            user: 'uzoanyadominic@gmail.com',
-            pass: 'VICEcity@47',
-        },
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+    const messageContent = `
+    <h3>YOU HAVE A NEW CONTACT REQUEST</h3>
+    <p>From: <strong>${req.body.name}</strong></p>
+    <p>Sender: <strong>${req.body.email}</strong></p>
+    <br>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    `;
+
+    sgMail.setApiKey('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');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    // const msg = {
+    //     from: `New Client`, // sender address
+    //     to: 'nomsouzoanya@yahoo.co.uk', // list of receivers
+    //     subject: 'Hello Dominic I Need Your Services', // Subject line
+    //     html: message // html body
+    //   };
+
+    const msg = {
+        to: 'nomsouzoanya@yahoo.co.uk',
+        from: req.body.email,
+        subject: 'Hello Dominic I Need Your Services',
+        text: messageContent
+        // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+      };
+
+      sgMail.send(msg).then(() => {
+          console.log('Message Sent Successfully');
+          res.status(200).end()
+      }).catch((err) => {
+            return console.log(err);
+      });
+
     // var transporter = nodemailer.createTransport({
     //     service: 'gmail',
     //     auth: {
@@ -80,30 +102,30 @@ app.post('/email', (req, res) => {
     //     }
     // });
     
-    transporter.verify(function(error, success) {
-        if (error) {
-             console.log(error);
-        } else {
-             console.log('Server is ready to take our messages');
-        }
-    });
-       // setup email data with unicode symbols
-    let mailOptions = {
-        from: `New Client`, // sender address
-        to: 'nomsouzoanya@yahoo.co.uk', // list of receivers
-        subject: 'Hello Dominic I Need Your Services', // Subject line
-        html: message // html body
-    };
+    // transporter.verify(function(error, success) {
+    //     if (error) {
+    //          console.log(error);
+    //     } else {
+    //          console.log('Server is ready to take our messages');
+    //     }
+    // });
+    //    // setup email data with unicode symbols
+    // let mailOptions = {
+    //     from: `New Client`, // sender address
+    //     to: 'nomsouzoanya@yahoo.co.uk', // list of receivers
+    //     subject: 'Hello Dominic I Need Your Services', // Subject line
+    //     html: message // html body
+    // };
       
 
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            return console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-            res.status(200).end();
-        }
-    });
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     if (error) {
+    //         return console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //         res.status(200).end();
+    //     }
+    // });
 });
 
 app.get((req, res) => {
